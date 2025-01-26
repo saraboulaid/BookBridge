59641eb246f837174a3a0173b2a4f6d95bb582a6
sara boulaid <sara24.blhd@gmail.com>
2025-01-26 12:39:00 +0100

    register&login

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserApp.Models;
+
+namespace UserApp.Data
+{
+	public class AppDbContext : IdentityDbContext<Users>
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserApp.Data;
+using UserApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddIdentity<Users, IdentityRole>(options =>
+{
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequiredLength = 8;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireLowercase = false;
+	options.User.RequireUniqueEmail = true;
+	options.SignIn.RequireConfirmedAccount = false;
+	options.SignIn.RequireConfirmedEmail = false;
+	options.SignIn.RequireConfirmedPhoneNumber = false;
+}
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+;
+var app = builder.Build();
+
+// Créer les rôles au démarrage
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await CreateRoles(roleManager);
+}
+
+async Task CreateRoles(RoleManager<IdentityRole> roleManager)
+{
+    string[] roleNames = { "Lecteur", "Bibliothécaire" };
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            var role = new IdentityRole(roleName);
+            await roleManager.CreateAsync(role);
+        }
+    }
+}
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
